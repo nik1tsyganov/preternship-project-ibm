d306c322d1594b86e2b181572465454121c0e8a4
Taylor Maida <tmaida@nd.edu>
2021-04-22 22:56:32 -0400

    Log Entry Class original

---
+++ logentry.py
@@ -0,0 +1,22 @@
+#Log Entry Class
+
+
+class LogEntry:
+
+	def __init__(self, index, status, data):
+		self.index = index
+		self.status = status
+		self.data = data 
+
+	def process(self):
+	# TODO : write function that process the data (string) to get the status of a given log entry
+	# can use self.data to access the string
+
+		#if ... :
+			#self.status == 'Error'
+		#elif ... :
+			#self.status == 'Warn'
+		#else:
+			#self.status == 'OK'
+
+
