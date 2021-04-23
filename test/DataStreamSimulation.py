71dee81ed2bf21a4f387d7864cbe9b5e8aa10580
nik1tsyganov <71796872+nik1tsyganov@users.noreply.github.com>
2021-04-23 17:08:07 -0400

    Updated DataStreamSimulation

--- test/DataStreamSimulation.py
+++ test/DataStreamSimulation.py
@@ -1,10 +1,15 @@
 # Reads in data from CLF file and outputs it like a data stream with timestamp
 import time
+import random
 
-with open('mock_data.txt', 'r') as i:
-    with open('new_file.txt', 'w') as o:
+RANGE = 1000
+
+with open('test_data.txt', 'r') as i:
+    with open('new_file.txt', 'a') as o:
         for line in i:
             currentTime = time.strftime('%H:%M:%S:%M')
             o.write(currentTime)
             o.write(' ')
             o.write(line)
+            o.flush()
+            time.sleep(random.randrange(RANGE)/RANGE)
